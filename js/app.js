32b0643eb5fd2fc20f35bd83706124506a3a6a28
Sergio <64972770+ssr765@users.noreply.github.com>
2023-11-28 21:22:57 +0100

    First try of making the union between nodes

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const id = Math.round(Math.random() * 10000);
 const node = document.getElementById('node');
+const union = document.querySelector('.union');
 console.info('WINDOW ID: ' + id);
 
 // Check if localStorage available.
@@ -45,8 +46,29 @@ function lookForDeadWindows() {
     }
 }
 
+/**
+ * Calculates the distance between nodes.
+ * @param {object} n1 This window node.
+ * @param {object} n2 Second window node.
+ * @returns {Number[]} The horizontal and vertical distance.
+ */
+function calculateDistance(n1, n2) {
+    return [n1.x - n2.x, n1.y - n2.y];
+}
+
+function calculateHipotenusa(adjacent, opposite) {
+    let hipotenusa = adjacent ** 2 + opposite ** 2;
+    return Math.sqrt(hipotenusa);
+}
+
+function calculateAngle(opposite, hipotenusa) {
+    let angle = opposite / hipotenusa;
+    return Math.round(Math.asin(angle) * 100);
+}
+
 let windows = loadStorage();
 let counter = 0;
+let nodes = 0;
 
 // Window initial data.
 const initialData = {'runtime': counter, 'x': 0, 'y': 0};
@@ -80,3 +102,34 @@ setInterval(() => {
 setInterval(() => {
     lookForDeadWindows();
 }, 499)
+
+// Make the union.
+setInterval(() => {
+    nodes = localStorage.length;
+    let node1;
+    let node2;
+
+    if (nodes === 2) {
+        
+        for (const [key, value] of Object.entries(windows)) {
+            if (key === `ventana${id}`) {
+                node1 = value;
+            } else {
+                node2 = value;
+            }
+        }
+        console.log(node1, node2);
+        let [adjacent, opposite] = calculateDistance(node1, node2);
+        let hipotenusa = calculateHipotenusa(adjacent, opposite);
+        let rotation = calculateAngle(opposite, hipotenusa);
+
+        console.log('top: ' + (node1.y - window.screenY));
+        console.log('left: ' + (node1.x - window.screenX));
+        union.style.width = `${Math.round(hipotenusa)}px`;
+        union.style.top = `${node1.y - window.screenY}px`;
+        union.style.left = `${node2.x - window.screenX}px`;
+        union.style.transform = `rotate(${rotation}deg)`
+
+    }
+
+}, 50)
